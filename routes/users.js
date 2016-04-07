56ba64613bbed14ce24a817ef4278d74cdc99d50
Bente80 <lions_bente@hotmail.com>
2016-04-07 15:01:53 +0200

    Added jwt security and token based security for API

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ var User = require('../app/model/user.js');
 var mongoose = require('mongoose');
 var config = require('../config/database');
 var passport = require('passport');
+var jwt = require('jwt-simple');
+const jwtConfig = require("../config/jwtConfig").jwtConfig;
+
 
 mongoose.connect(config.database); // connect to database
 require('../config/passport')(passport); // pass passport for configuration
@@ -15,11 +18,11 @@ router.get('/', function(req, res, next) {
 
 // create a new user account (POST http://localhost:8080/api/signup)
 router.post('/signup', function(req, res) {
-  if (!req.body.name || !req.body.password) {
+  if (!req.body.username || !req.body.password) {
        res.json({success: false, msg: 'Please pass name and password.'});
   } else {
     var newUser = new User({
-      userName: req.body.name,          // name changed to username.----
+      userName: req.body.username,          // name changed to username.----
       password: req.body.password
     });
     // save the user
@@ -30,6 +33,37 @@ router.post('/signup', function(req, res) {
       res.json({success: true, msg: 'Successful created new user.'});
     });
   }
+});
+
+router.post('/authenticate', function(req, res) {
+  User.findOne({
+    userName: req.body.username
+  }, function (err, user) {
+    if (err) throw err;
+    if (!user) {
+      res.status(401).send({ msg: 'Authentication failed. User not found.'});
+    } else {
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          var iat = new Date().getTime()/1000; //convert to seconds
+          var exp = iat+jwtConfig.tokenExpirationTime;
+          var payload = {
+            aud: jwtConfig.audience,
+            iss: jwtConfig.issuer,
+            iat: iat,
+            exp: exp,
+            sub: user.userName
+          }
+          var token = jwt.encode(payload, jwtConfig.secret);
+          // return the information including token as JSON
+          res.json({token: 'JWT ' + token});
+        } else {
+          res.status(401).send({ msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
